bdd152aa06ddbfb28d063095f3b98c443c76d1ac
Zachary Schulz <zjschulz@gmail.com>
2019-08-31 00:05:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages][1]
+programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
