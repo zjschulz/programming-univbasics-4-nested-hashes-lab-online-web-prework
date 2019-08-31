d2795aa28921715bfe4148ecc0efca416fe7b02c
Zachary Schulz <zjschulz@gmail.com>
2019-08-31 00:41:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,8 +81,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]
-programmer_hash
+programmer_hash[:yukihiro_matsumoto] = "Value"
 end
 
 def changing_alan
